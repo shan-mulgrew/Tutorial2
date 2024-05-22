4651e1a97e617ed0876987061423514f2c7f4cce
Shan Mulgrew <shannon.mulgrew@inventise.com>
2024-05-22 13:22:16 +0100

    finished section 4

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgIf, UpperCasePipe} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {Hero} from '../hero';
 
 @Component({
+  standalone: false,
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrl: './hero-detail.component.css'
+  styleUrls: ['./hero-detail.component.css'],
 })
 export class HeroDetailComponent {
-
+  @Input() hero?: Hero;
 }
